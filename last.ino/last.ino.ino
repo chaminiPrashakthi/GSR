1c85423d4173385e6868570f4201babea878583f
Prashakthi <webchamzz@gmail.com>
2017-06-01 17:53:58 +0530

    Add files via upload

---
+++ last.ino/last.ino.ino
@@ -0,0 +1,18 @@
+
+/* GSR Sensor data reading */
+int GSR= A2;
+int sensorValue;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(A2,INPUT);
+
+  }
+
+void loop() {
+  int temp;
+  sensorValue=analogRead(GSR);
+  Serial.print("Sessor Value =");
+  Serial.println(sensorValue);
+}
